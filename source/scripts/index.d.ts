26a26f7d2ddfe23f97c26c6afbe6109ba360ce9b
Danny Guo <dannyguo91@gmail.com>
2021-11-07 14:58:37 -0500

    Remove the font-color-contrast types
    The library includes the types now:
    https://github.com/russoedu/font-color-contrast/pull/1
    
    And was rewritten in TypeScript:
    https://github.com/russoedu/font-color-contrast/pull/3

--- source/scripts/index.d.ts
+++ source/scripts/index.d.ts
@@ -1,19 +1,3 @@
-declare module "font-color-contrast" {
-    type ColorIntensity = number | string;
-
-    function fontColorContrast(hex: string): string;
-    function fontColorContrast(
-        red: ColorIntensity,
-        green: ColorIntensity,
-        blue: ColorIntensity
-    ): string;
-    function fontColorContrast(
-        redGreenBlue: [ColorIntensity, ColorIntensity, ColorIntensity]
-    ): string;
-
-    export = fontColorContrast;
-}
-
 declare module "hsv-rgb" {
     function hsvToRgb(
         hue: number,
